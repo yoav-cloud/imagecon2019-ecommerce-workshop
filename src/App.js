50cd9cb974f71df90d225b54fff51274e4ce6a48
nadavofi <nadav.ofir@cloudinary.com>
2019-02-26 16:34:37 +0200

    conflicts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React  from "react";
+import React from "react";
 import { inject } from "mobx-react";
 import { Switch, Route } from "react-router";
 import { Link } from "react-router-dom";
 import Admin from "./pages/Admin/Admin";
 import ProductListingPage from "./pages/ProductListing/productListing";
 import ProductPage from "./components/Product/Product";
-
 
 const About = inject("routing")(props => {
   console.log("!!!!!!!!!!!! ABOUT PROPS = ", props);
@@ -22,19 +21,15 @@ const About = inject("routing")(props => {
 });
 
 const App = () => (
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={ProductListingPage} />
-          <Route path="/product" component={About} />
-          <Route path="/admin" component={Admin} />
-<<<<<<< HEAD
-          <Route path="/product" component={ProductPage} />
-          {/*<Route path="/:user" component={User}/>*/}
-          {/*<Route component={NoMatch}/>*/}
-=======
->>>>>>> bae392ef4f4a2642c25b5e870c2fe269fbc3562b
-        </Switch>
-      </div>
-    );
+  <div className="App">
+    <Switch>
+      <Route exact path="/" component={ProductListingPage} />
+      <Route path="/product" component={ProductPage} />
+      <Route path="/admin" component={Admin} />
+      {/*<Route path="/:user" component={User}/>*/}
+      {/*<Route component={NoMatch}/>*/}
+    </Switch>
+  </div>
+);
 
 export default inject("appData")(App);
