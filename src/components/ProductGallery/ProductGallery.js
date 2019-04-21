16a8279bee9d24bba55937b653b5edc2db427424
yoav niran <yoav@cloudinary.com>
2019-04-21 14:36:43 +0300

    gallery wip

--- src/components/ProductGallery/ProductGallery.js
+++ src/components/ProductGallery/ProductGallery.js
@@ -1,19 +1,13 @@
 import React from "react";
 
-const ProductGallery = ({ image }) => {
+const ProductGallery = ({ mediaAssets }) => {
+
+	console.log("!!!!!!!!! MEDIA ASSETS -- ", mediaAssets);
+
 	return (
 		<div className="image-gallery " aria-live="polite">
 			<div className="image-gallery-content">
-				<div className="image-gallery-slide-wrapper left">
-					<div className="image-gallery-slides">
-						<div className="image-gallery-slide center"
-						     role="button"
-						     style={{ transform: "translate3d(0%, 0px, 0px)" }}>
-							<div className="image-gallery-image"><img
-								src={image} alt=""/></div>
-						</div>
-					</div>
-				</div>
+
 			</div>
 		</div>
 	);
--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,10 +1,14 @@
 import React, { Fragment } from "react";
-// import { camelCase } from "lodash";
+import { camelCase } from "lodash";
 import Breadcrumb from "../../components/Breadcrumb/Breadcrumb";
 import ProductGallery from "../../components/ProductGallery/ProductGallery";
 import Description from "./Description";
 import Related from "./Related";
 import Info from "./Info";
+
+const getMediaAssets = (product) =>
+	product.items.map(item =>
+		Object.keys(item).map((key) => camelCase(key)));
 
 const ProductDetail = ({ location }: Object) => {
 	const product = location.state ? location.state.product : {};
@@ -15,8 +19,8 @@ const ProductDetail = ({ location }: Object) => {
 				<div className="container">
 					<div className="columns">
 						<div className="column is-7">
-							<Breadcrumb/>
-							<ProductGallery image={product.image}/>
+							<Breadcrumb />
+							<ProductGallery mediaAssets={getMediaAssets(product)} />
 						</div>
 						<div className="column is-5">
 							<Info product={product}/>
@@ -31,6 +35,3 @@ const ProductDetail = ({ location }: Object) => {
 };
 
 export default ProductDetail;
-
-// <ProductGallery mediaAssets={product.items.map(item =>
-// Object.keys(item).map((key) => camelCase(key)))} />
