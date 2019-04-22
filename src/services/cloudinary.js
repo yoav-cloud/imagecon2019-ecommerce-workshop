2072690442e8e48bc57678c865a098cb26a592e5
yoav niran <yoav@cloudinary.com>
2019-04-22 15:31:14 +0300

    list: responsive images and sale badge

--- src/services/cloudinary.js
+++ src/services/cloudinary.js
@@ -1,19 +1,25 @@
 import { Cloudinary } from "cloudinary-core";
 import { generateImageResponsiveAttributes } from "cloudinary/lib/utils/srcsetUtils";
-
 import { CLOUD } from "../consts";
 
 const cldInstance = Cloudinary.new({ cloud_name: CLOUD });
 
+//so React doesnt complain
+const replaceSrcsetProp = (obj) => {
+	obj.srcSet = obj.srcset;
+	delete obj.srcset;
+	return obj;
+};
+
 export const getResponsiveAttributes = (id, min, max, transformation) =>
-	generateImageResponsiveAttributes(id, {}, {
+	replaceSrcsetProp(generateImageResponsiveAttributes(id, {}, {
 		sizes: true,
 		min_width: min,
 		max_width: max,
 		max_images: 4,
-		transformation: {transformation},
+		transformation: { transformation },
 	}, {
 		cloud_name: CLOUD,
-	});
+	}));
 
 export default cldInstance;
