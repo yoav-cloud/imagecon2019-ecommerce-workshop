aaa5634fc973f9ab4be232a7ee62faf34e2e40ad
yoav niran <yoav@cloudinary.com>
2019-04-22 15:45:38 +0300

    pass media assets to product gallery

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -8,8 +8,6 @@ import Info from "./Info";
 const ProductDetail = ({ location }: Object) => {
 	const product = location.state ? location.state.product : {};
 
-	const imageItem = product.items.find((item) => item.resourceType === "image");
-
 	return (
 		<Fragment>
 			<section className="section section-product">
@@ -17,7 +15,7 @@ const ProductDetail = ({ location }: Object) => {
 					<div className="columns">
 						<div className="column is-7">
 							<Breadcrumb />
-							<ProductGallery image={imageItem.secureUrl} />
+							<ProductGallery mediaAssets={product.items} />
 						</div>
 						<div className="column is-5">
 							<Info product={product}/>
