79469e896b04cd59764080c10494ca183191e693
yoav niran <yoav@cloudinary.com>
2019-04-23 15:54:11 +0300

    listing snippets

--- src/components/GridItem/GridImage.js
+++ src/components/GridItem/GridImage.js
@@ -1,10 +1,17 @@
 import React from "react";
+// impcldres :: import cloudinary & responsive helper
 
 const GridImage = ({ item }) => {
+
+	// gridtrans :: transformation for grid image
+
+	// respattrs :: create responsive attributes
+
+	// cldurl :: generate url
 	const url = item.secureUrl;
 
 	return (
-		<img src={url} alt={item.publicId}/>
+		<img src={url} alt={item.publicId} /*{...respAttrs}*/ />
 	);
 };
 
